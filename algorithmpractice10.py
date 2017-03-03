fedb24b305cf5743a2ba9069d71469192cf0f0b8
Sarah Basinger <sarrey@gmail.com>
2017-03-03 11:22:14 -0500

    lattice path algorithm

--- algorithmpractice10.py
+++ algorithmpractice10.py
@@ -1,3 +1,8 @@
+# Starting in the top left corner of a 2×2 grid, 
+# and only being able to move to the right and down, 
+# there are exactly 6 routes to the bottom right corner.
+# How many such routes are there through a 20×20 grid?
+
 def find_routes(rows,columns):
 	dictionary = {}
 	for i in range (0,rows+1):
